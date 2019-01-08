04aa4b135135e2f5bc7749bbc1e53c3063f7df40
Ngo Van Tuan <contact.tuannv@gmail.com>
2019-01-08 09:48:28 +0700

    Update Theme

---
+++ AssignmentOauth2Server/Models/CreateAccount.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AssignmentOauth2Server.Models
+{
+    public class CreateAccount
+    {
+        public AccountInfomation AccountInfomation { get; set; }
+
+        public Role Role { get; set; }
+
+        public Class Class { get; set; }
+    }
+}
